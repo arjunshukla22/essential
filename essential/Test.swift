e51842b51fc01f7fe48dbc1eb25772f0da4c8d32
Arjun Shukla <40709718+arjunshukla22@users.noreply.github.com>
2022-06-29 14:52:00 +0530

    First Comit
    First Commit

---
+++ essential/Test.swift
@@ -0,0 +1,24 @@
+//
+//  Test.swift
+//  essential
+//
+//  Created by Arjun iOS  on 29/06/22.
+//
+
+import Foundation
+import UIKit
+
+
+public final class SwiftyLib {
+
+    let name = "SwiftyLib"
+    
+    public func add(a: Int, b: Int) -> Int {
+        return a + b
+    }
+    
+    public func sub(a: Int, b: Int) -> Int {
+        return a - b
+    }
+    
+}
---
+++ essential/essential.h
@@ -0,0 +1,18 @@
+//
+//  essential.h
+//  essential
+//
+//  Created by Arjun iOS  on 29/06/22.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for essential.
+FOUNDATION_EXPORT double essentialVersionNumber;
+
+//! Project version string for essential.
+FOUNDATION_EXPORT const unsigned char essentialVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <essential/PublicHeader.h>
+
+
